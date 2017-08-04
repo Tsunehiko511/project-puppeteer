039c8c9272ce53b9d9ea5bc8d62f0eff9154e5e4
ROOT7 <shigesaku.390@gmail.com>
2017-08-04 23:04:41 +0900

    やっつけランダムアルゴリズムver.1 作成者：ROOT7

---
+++ Menu/Script/ROOT7.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROOT7 : MonoBehaviour {
+
+  //世代変数
+  int generation = 0;
+  int generation_max = 100; // こいつを小さくすると処理の時間が短くなるかも
+
+  Player player = new Player();   //こいつを鍛える（？）
+  Player enemy = new Player();    //こいつに勝てる個体を見つける
+
+  bool evaluations = true; // 評価（今回はとにかくランダムなので評価は勝つ[false]か負ける[true]かのみ！）
+
+  AI_Database ai_db = new AI_Database();
+  System.Random cRandom = new System.Random();
+
+  public ROOT7() {
+    // 登場人物はあらかじめ生成しておく
+    player.SetData(ai_db.GetUnits(""), ai_db.GetCodes(""));
+
+    // Plan0のPlan内設定をランダムに設定する
+    Initial_Setting();
+  }
+
+  // 初期設定
+  void Initial_Setting() {
+    // 子供をランダム設定
+    SetInitRandom(player);
+
+    // 評価する時に対戦する相手の設定
+    enemy.SetData(ai_db.GetUnits("第7回(fred)"), ai_db.GetCodes("第7回(fred)"));
+  }
+
+  // Plan0のPlan内設定をランダム生成・Plan変更設定はリセット
+  void SetInitRandom(Player _player) {
+    // 今回はキング，クイーン，ナイト，ビショップで固定
+    _player.Units = new int[4] { 0, 1, 2, 3 };
+
+    // 4体のPlan0のデータを初期設定する i=ユニットのid, j=Plan0の行のid
+    for (int i = 0; i < 4; i++) {
+      // Plan0のPlan変更設定を初期にする
+      for (int j = 0; j < 5; j++) {
+        _player.plans[i].GetPlan(0).transitions.SetRow(j, new int[3] { 0, 0, 0 }); // Plan変更設定を初期にする=Plan変更しない
+        _player.plans[i].GetPlan(0).elements.SetRow(j, GetPlanElementCode()); // Plan内設定にランダムな行を入れる
+      }
+    }
+  }
+
+  // AIのデータベースからPlan内設定の行をランダムに取得する。
+  int[] GetPlanElementCode() {
+    List<int[]> tmp_list = ai_db.GetPlanSettingCodeRowList();
+    int rndm = cRandom.Next(tmp_list.Count);
+    return tmp_list[rndm];
+  }
+
+  public void Play(GameMaster _gamemaster) {
+
+    // 勝つまで立ち上がれ！
+    while (evaluations) {
+
+      //世代交代
+      generation++;
+      SetInitRandom(player);
+
+      // 対戦に勝利すればRED，負ければBLUEを取得する
+      string tmp_win = _gamemaster.Play(player, enemy);
+
+      // 勝ったね！やった！
+      if (tmp_win == "RED") {
+        evaluations = false;
+      }
+
+      if (generation >= generation_max) { // フリーズするので一定数を超えると処理を終える
+        Debug.Log("むりだようかてないよう");
+        break;
+      }
+    }
+
+    if (!evaluations) {
+      //output
+      Debug.Log(generation + "体目");
+      Debug.LogFormat("勝者：{0}  詳細(赤,青)：残機({1},{2})，キングHP({3}, {4})", _gamemaster.Play(player, enemy), _gamemaster.GetResult("RED_LEFT"), _gamemaster.GetResult("BLUE_LEFT"), _gamemaster.GetResult("RED_KING_HP"), _gamemaster.GetResult("BLUE_KING_HP"));
+      Debug.Log(player.plans[0].SaveToString() + "\n"
+        + player.plans[1].SaveToString() + "\n"
+        + player.plans[2].SaveToString() + "\n"
+        + player.plans[3].SaveToString());
+    }
+
+    Debug.Log("終了");
+  }
+}
--- Menu/Script/Setting.cs
+++ Menu/Script/Setting.cs
@@ -2,47 +2,52 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class Setting : MonoBehaviour {
-	GameMaster Game;
-	Player player1;
-	Player player2;
-	AI_Database ai_db;
-	GA_2NE_ORIGIN ga_2ne_origin;
-	void Start(){
-		ai_db = new AI_Database();
-		Game = new GameMaster();
-		player1 = new Player();
-		player2 = new Player();
-		ga_2ne_origin = new GA_2NE_ORIGIN();
-	}
+  GameMaster Game;
+  Player player1;
+  Player player2;
+  AI_Database ai_db;
+  GA_2NE_ORIGIN ga_2ne_origin;
+  ROOT7 Root7;
+  void Start() {
+    ai_db = new AI_Database();
+    Game = new GameMaster();
+    player1 = new Player();
+    player2 = new Player();
+    ga_2ne_origin = new GA_2NE_ORIGIN();
+    Root7 = new ROOT7();
+  }
 
-	// ボタンを押すとこの関数を実行する
-	public void PushTestButton(){
-		TestPlay("TEST");
-	}
+  // ボタンを押すとこの関数を実行する
+  public void PushTestButton() {
+    TestPlay("ROOT7");
+  }
 
-	// 数々のテストプレイ
-	void TestPlay(string _name){
-		switch(_name){
-			case "TEST":// とりあえず対戦させる
-			// player1とplayer2にデータをセットする。SetData(セットするプレイヤ，ユニットの種類の配列，AIの配列);
-			player1.SetData(ai_db.GetUnits(ai_db.GetName(7)), ai_db.GetCodes(ai_db.GetName(7)));
-			player2.SetData(ai_db.GetUnits(ai_db.GetName(10)), ai_db.GetCodes(ai_db.GetName(10)));
-			//Game.Play(赤, 青)：1回処理するのに0.02秒程度かかる
-			for(int i=0; i<1; i++){
-				Debug.LogFormat("勝者：{0}  詳細(赤,青)：残機({1},{2})，キングHP({3}, {4})", Game.Play(player1, player2), Game.GetResult("RED_LEFT"), Game.GetResult("BLUE_LEFT"), Game.GetResult("RED_KING_HP"), Game.GetResult("BLUE_KING_HP"));
-				Debug.LogFormat("勝者：{0}  詳細(赤,青)：残機({1},{2})，キングHP({3}, {4})", Game.Play(player2, player1), Game.GetResult("RED_LEFT"), Game.GetResult("BLUE_LEFT"), Game.GetResult("RED_KING_HP"), Game.GetResult("BLUE_KING_HP"));
-			}
-			break;
-			case "GA_2NE_ORIGIN":// 2neが開発した遺伝的アルゴリズム ボタンを押してから数十秒処理するからご注意を！
-			ga_2ne_origin.Play(Game);
-			break;
-			case "":// 後の人はここにファイル名を入れてね
-			// ここに処理を書いてね
-			break;
-			default:
-			break;
-		}
-	}
+  // 数々のテストプレイ
+  void TestPlay(string _name) {
+    switch (_name) {
+      case "TEST":// とりあえず対戦させる
+                  // player1とplayer2にデータをセットする。SetData(セットするプレイヤ，ユニットの種類の配列，AIの配列);
+        player1.SetData(ai_db.GetUnits(ai_db.GetName(7)), ai_db.GetCodes(ai_db.GetName(7)));
+        player2.SetData(ai_db.GetUnits(ai_db.GetName(10)), ai_db.GetCodes(ai_db.GetName(10)));
+        //Game.Play(赤, 青)：1回処理するのに0.02秒程度かかる
+        for (int i = 0; i < 1; i++) {
+          Debug.LogFormat("勝者：{0}  詳細(赤,青)：残機({1},{2})，キングHP({3}, {4})", Game.Play(player1, player2), Game.GetResult("RED_LEFT"), Game.GetResult("BLUE_LEFT"), Game.GetResult("RED_KING_HP"), Game.GetResult("BLUE_KING_HP"));
+          Debug.LogFormat("勝者：{0}  詳細(赤,青)：残機({1},{2})，キングHP({3}, {4})", Game.Play(player2, player1), Game.GetResult("RED_LEFT"), Game.GetResult("BLUE_LEFT"), Game.GetResult("RED_KING_HP"), Game.GetResult("BLUE_KING_HP"));
+        }
+        break;
+      case "GA_2NE_ORIGIN":// 2neが開発した遺伝的アルゴリズム ボタンを押してから数十秒処理するからご注意を！
+        ga_2ne_origin.Play(Game);
+        break;
+      case "ROOT7"://ROOT7のランダムアルゴリズム　処理時間多くて2分以内
+        Root7.Play(Game);      // ここに処理を書いてね
+        break;
+      case "":// 後の人はここにファイル名を入れてね
+
+        break;
+      default:
+        break;
+    }
+  }
 }
 // 時間計測
 // float start_time = Time.time;
